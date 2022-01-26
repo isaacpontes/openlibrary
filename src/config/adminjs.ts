c19a018aedbd7b3dfb2df32ce29dab75ce68ea57
Isaac L. F. Pontes <isaaclfp@outlook.com>
2022-01-26 10:39:57 -0300

    Added custom colors

--- src/config/adminjs.ts
+++ src/config/adminjs.ts
@@ -35,7 +35,19 @@ const adminJs = new AdminJs({
     branding: {
         companyName: 'OpenLibrary',
         theme: {
-            colors: { }
+            colors: {
+                primary100: '#C84B31',
+                primary80: '#d66c57',
+                primary60: '#db7e6b',
+                primary40: '#e09080',
+                primary20: '#eab4a9',
+                grey100: '#191919',
+                grey80: '#333333',
+                grey60: '#4d4d4d',
+                hoverBg: '#545454',
+                filterBg: '#2D4263',
+                accent: '#eab4a9'
+            }
         }
     },
     locale: locale
